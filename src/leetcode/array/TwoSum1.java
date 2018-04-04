c789eacf484a7ee6a5a042d3f4fe21d267223fee
ohugh <158536234@qq.com>
2018-04-04 14:12:15 +0800

    提交了array中的 1.TwoSum

---
+++ src/leetcode/array/TwoSum1.java
@@ -0,0 +1,59 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//效率太低
+//class Solution {
+//    public int[] twoSum(int[] nums, int target) {
+//    	int[] result=new int[2];
+//    	
+//    	if(nums.length<2)
+//    		return result;
+//    	
+//    	for (int i = 0; i < nums.length; i++) {
+//			for (int j = i+1; j < nums.length; j++) {
+//				if(nums[i]+nums[j]==target) {
+//					result[0]=i;
+//					result[1]=j;
+//				}
+//			}
+//		}
+//    	return result;
+//    }
+//}
+
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+       int[] result = new int[2];
+	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for (int i = 0; i < numbers.length; i++){
+	        if (map.containsKey(target - numbers[i])) {
+	            result[1] = i;
+	            result[0] = map.get(target - numbers[i]);
+	            return result;
+	        }
+	        map.put(numbers[i], i);
+	    }
+	    return result;
+    }
+}
+
+
+
+public class TwoSum1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s= new Solution();
+		int[] num= {2,7,11,15};
+		int[] res=s.twoSum(num, 9);
+		for(int i:res) {
+			System.out.println(i);
+		}
+	}
+}
